8b1fe2b0ba0c26100cb6d89543181f4f165c125a
allan <epicallan.al@gmail.com>
2016-09-01 21:47:42 +0300

    minor changes :dash:

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
-/**
- * Module dependencies.
- */
 const app = require('./src/app');
-
 /**
  * Get port from environment and store in Express.
  */
 const PORT = process.env.PORT || '7000';
 // Use the Express application instance to listen to the '3000' port
 app.listen(PORT);
-
 // Log the server status to the console
 console.log(`Server running at ${PORT}`);
